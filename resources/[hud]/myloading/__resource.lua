7e790092f378f904ba8b698fe5770a1df18e8ad6
Nikolas Snorkell <pushok_nik@mail.ru>
2021-06-08 16:35:27 +0300

    added chat/dist

--- resources/[hud]/myloading/__resource.lua
+++ resources/[hud]/myloading/__resource.lua
@@ -1,12 +1,12 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
 files {	
     'index.html',
     'css/main.css',
     'img/loading2.gif',
-    'img/cursor.png',
     'music/music.mp3',
     'js/script.js'
     
 }
 
 loadscreen 'index.html'
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
